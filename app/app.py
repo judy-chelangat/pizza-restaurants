9b969ed34dc6c0f01c75df2d2b6993e8b0654195
judy-chelangat <judysigilai75@gmail.com>
2023-09-23 14:30:59 +0300

    added a restaurants route to get all the restuarant details

--- app/app.py
+++ app/app.py
@@ -13,8 +13,26 @@ migrate=Migrate(app,db)
 
 db.init_app(app)
 
+#creating the routes 
+#home route
+@app.route('/')
+def home():
+  return '<h1>Welcome to restaurants</h1>'
 
+@app.route('/restaurants',methods=['GET'])
+def restaurants():
+  restaurants=Restaurant.query.all()
+  restaurants_data=[] #list to store the data
+
+  for restaurant in restaurants:
+    restaurant_details={
+            "id": restaurant.id,
+            "name": restaurant.name,
+            "address": restaurant.address
+    }
+    restaurants_data.append(restaurant_details)
+    response=make_response(jsonify(restaurants_data),200)
+  return response
 
 if __name__=='__main__':
   app.run(port=5555,debug=True)
-  
