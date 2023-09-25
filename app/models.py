352e07b700e3cb4d9ad336230facd57057acb5a1
judy-chelangat <judysigilai75@gmail.com>
2023-09-25 11:50:48 +0300

    added comments

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy_serializer import SerializerMixin
+#from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.orm import validates
 
 db=SQLAlchemy() #Setting up the db 
